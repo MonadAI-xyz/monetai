502e83f7a26512f5d293dcb0159cba8c6bfd52f6
Tayyab Ali <Tayyab Ali>
2025-03-04 22:00:13 +0500

    refactor: remove Base from networks list; refactor: made Monad Testnet selected by default

--- frontend/src/config/wagmi.ts
+++ frontend/src/config/wagmi.ts
@@ -1,7 +1,6 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import {
   mainnet,
-  base,
   monadTestnet,
 } from 'wagmi/chains';
 
@@ -9,12 +8,11 @@ export const wagmiConfig =  getDefaultConfig({
   appName: 'MonetAI',
   projectId: process.env.NEXT_PUBLIC_RAINBOW_PROJECT_ID,
   chains: [
-    mainnet,
-    base,
     {
       ...monadTestnet,
       iconUrl: 'https://api.phantom.app/image-proxy/?image=https%3A%2F%2Fdhc7eusqrdwa0.cloudfront.net%2Fassets%2Fmonad.png&anim=true&fit=cover&width=176&height=176',
-    }
+    },
+    mainnet,
   ],
   ssr: true, // If your dApp uses server side rendering (SSR)
 });
