f7b2c52d0d149426bd1f6f663fa477e561790bf6
Tayyab Ali <Tayyab Ali>
2025-03-07 23:13:37 +0500

    refactor: update connect wallet button color

--- frontend/src/providers/wallet-provider.tsx
+++ frontend/src/providers/wallet-provider.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { darkTheme, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { darkTheme, RainbowKitProvider, Theme } from '@rainbow-me/rainbowkit';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import merge from 'lodash.merge';
 import React from 'react';
 import { WagmiProvider } from 'wagmi';
 
@@ -9,11 +10,19 @@ import { wagmiConfig } from '@/config/wagmi';
 
 const queryClient = new QueryClient();
 
+// Extending RainbowKitProvider's built-in theme
+const rainbowKitCustomTheme = merge(darkTheme(), {
+  colors: {
+    accentColor: '#816cf9', // TODO - define color variable in tailwindcss
+    // connectButtonBackground: '#816cf9',
+  },
+} as Theme);
+
 export default function WalletProvider({ children }: { children: React.ReactNode }) {
   return (
     <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider modalSize="compact" theme={darkTheme()}>
+        <RainbowKitProvider modalSize="compact" theme={rainbowKitCustomTheme}>
           {children}
         </RainbowKitProvider>
       </QueryClientProvider>
