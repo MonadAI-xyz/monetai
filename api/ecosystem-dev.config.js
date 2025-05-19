10a9826d31d8711eb677afd09cbc26505b4b2c9b
Hamza Awan <mh89371@gmail.com>
2025-05-19 20:40:01 +0500

    Fixing X Service Issues and Deployment Issues

--- api/ecosystem-dev.config.js
+++ api/ecosystem-dev.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [{
       name: 'monetai-dev',
       script: 'build/server.js',
-      instances: 2,
+      instances: 1,
       exec_mode: 'cluster',
       autorestart: true,
       watch: false,
@@ -10,8 +10,8 @@ module.exports = {
       env: {
         NODE_ENV: 'development'
       },
-      output: '/var/log/monetai-prod/out.log',
-      error: '/var/log/monetai-prod/error.log',
+      output: '/var/log/monetai-dev/out.log',
+      error: '/var/log/monetai-dev/error.log',
       log_date_format: "YYYY-MM-DD HH:mm:ss"
     }]
   }
